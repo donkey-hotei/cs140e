905e3fb67bb9b1173e152526677d930f0765385a
donkey_hotei <donkey-hotei@cryptolab.net>
2018-08-19 14:13:08 -0700

    add solution to phase3 of assignment 0

--- phase3/blinky.c
+++ phase3/blinky.c
@@ -1,4 +1,5 @@
 #define GPIO_BASE (0x3F000000 + 0x200000)
+#define true 1
 
 volatile unsigned *GPIO_FSEL1 = (volatile unsigned *)(GPIO_BASE + 0x04);
 volatile unsigned *GPIO_SET0  = (volatile unsigned *)(GPIO_BASE + 0x1C);
@@ -16,5 +17,12 @@ static void spin_sleep_ms(unsigned int ms) {
 
 int main(void) {
   // STEP 1: Set GPIO Pin 16 as output.
+  *GPIO_FSEL1 |= (0x01 << 0x12);
   // STEP 2: Continuously set and clear GPIO 16.
+  while (true) {
+    *GPIO_SET0 |= 0x01 << 0x10;
+    spin_sleep_ms(256);
+    *GPIO_CLR0 |= 0x01 << 0x10;
+    spin_sleep_ms(256);
+  }
 }
