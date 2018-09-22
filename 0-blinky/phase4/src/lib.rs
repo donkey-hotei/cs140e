03fc63300d61fbc8116197d4b759cbe1ce154eae
donkey_hotei <donkey-hotei@cryptolab.net>
2018-09-22 11:58:41 -0700

    move all files into 0-blinky subdir

--- 0-blinky/phase4/src/lib.rs
+++ 0-blinky/phase4/src/lib.rs
@@ -7,8 +7,8 @@ pub mod lang_items;
 const GPIO_BASE: usize = 0x3F000000 + 0x200000;
 
 const GPIO_FSEL1: *mut u32 = (GPIO_BASE + 0x04) as *mut u32;
-const GPIO_SET0: *mut u32 = (GPIO_BASE + 0x1C) as *mut u32;
-const GPIO_CLR0: *mut u32 = (GPIO_BASE + 0x28) as *mut u32;
+const GPIO_SET0: *mut u32  = (GPIO_BASE + 0x1C) as *mut u32;
+const GPIO_CLR0: *mut u32  = (GPIO_BASE + 0x28) as *mut u32;
 
 #[inline(never)]
 fn spin_sleep_ms(ms: usize) {
