e5dd22da14f7c3a976d072379725370b7e23fe0a
donkey_hotei <donkey-hotei@cryptolab.net>
2018-09-24 16:24:08 -0700

    implement max for PartialOrd types

--- 1-shell/ferris-wheel/run-pass/expressions.rs
+++ 1-shell/ferris-wheel/run-pass/expressions.rs
@@ -5,6 +5,16 @@
 #[derive(Debug, PartialOrd, PartialEq, Clone, Copy)]
 struct IntWrapper(isize);
 
+fn max<T>(v1: T, v2: T) -> T where T: PartialOrd {
+    if v1 > v2 {
+        v1
+    } else if v1 < v2 {
+        v2
+    } else {
+        v1
+    }
+}
+
 pub fn main() {
     assert_eq!(max(1usize, 3), 3);
     assert_eq!(max(1u8, 3), 3);
