f5934b8abd024f6653b40dadb771626bb4903664
donkey_hotei <donkey-hotei@cryptolab.net>
2018-09-25 11:38:15 -0700

    implement PartialEq trait for Duration

--- 1-shell/ferris-wheel/run-pass/trait-impl.rs
+++ 1-shell/ferris-wheel/run-pass/trait-impl.rs
@@ -1,9 +1,27 @@
 // FIXME: Make me pass! Diff budget: 25 lines.
-
+#[derive(Debug)]
 enum Duration {
     MilliSeconds(u64),
     Seconds(u32),
     Minutes(u16)
+}
+
+use Duration::{Seconds, MilliSeconds, Minutes};
+
+impl Duration {
+    fn to_minutes(&self) -> u64 {
+        match self {
+            MilliSeconds(value) => *value / 60_000,
+            Seconds(value)      => *value as u64 / 60,
+            Minutes(value)      => *value as u64
+        }
+    }
+}
+
+impl PartialEq for Duration {
+    fn eq(&self, other: &Duration) -> bool {
+        self.to_minutes() == other.to_minutes()
+    }
 }
 
 fn main() {
