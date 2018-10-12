c92791618344d56c17b0fe1a5cbfc3a03cc85c38
donkey_hotei <donkey-hotei@cryptolab.net>
2018-10-11 17:06:07 -0700

    finish ttywrite implementation

--- 1-shell/ttywrite/src/main.rs
+++ 1-shell/ttywrite/src/main.rs
@@ -7,11 +7,17 @@ use std::path::PathBuf;
 use std::time::Duration;
 
 use structopt::StructOpt;
-use serial::core::{CharSize, BaudRate, StopBits, FlowControl, SerialDevice, SerialPortSettings};
+use serial::core::{
+    CharSize, 
+    BaudRate, 
+    StopBits, 
+    FlowControl, 
+    SerialDevice, 
+    SerialPortSettings,
+};
 use xmodem::{Xmodem, Progress};
 
 mod parsers;
-
 use parsers::{parse_width, parse_stop_bits, parse_flow_control, parse_baud_rate};
 
 #[derive(StructOpt, Debug)]
@@ -47,12 +53,45 @@ struct Opt {
     raw: bool,
 }
 
+
+fn progress_fn(progress: Progress) {
+    println!("Progress: {:?}", progress);
+}
+
+
 fn main() {
     use std::fs::File;
-    use std::io::{self, BufReader, BufRead};
+    use std::io::{self, Read};
 
     let opt = Opt::from_args();
     let mut serial = serial::open(&opt.tty_path).expect("path points to invalid TTY");
 
-    // FIXME: Implement the `ttywrite` utility.
+    serial.set_timeout(Duration::from_secs(opt.timeout)).is_ok();
+
+    let mut settings = serial.read_settings().unwrap();
+
+    settings.set_baud_rate(opt.baud_rate).is_ok();
+    settings.set_char_size(opt.char_width);
+    settings.set_flow_control(opt.flow_control);
+    settings.set_stop_bits(opt.stop_bits);
+
+    let mut buffer: Vec<u8> = vec![];
+    let bytes_read = match opt.input {
+        None => io::stdin().read_to_end(&mut buffer).expect("failed reading from stdin"),
+        Some(ref input) => {
+            let mut file = File::open(input).expect("file not found");
+            file.read_to_end(&mut buffer).expect("failed reading from input")
+        }
+    };
+
+    println!("[+] wrote {:?} bytes to input", bytes_read);
+
+    let mut input = io::Cursor::new(buffer);
+    if opt.raw {
+        io::copy(&mut input, &mut serial)
+            .expect("failed to copy data from input to tty");
+    } else {
+        Xmodem::transmit_with_progress(input, serial, progress_fn)
+            .expect("failed to send data via xmodem");
+    }
 }
